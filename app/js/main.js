244d3ab0c900987356750b56585463707914955e
LatyshevIV <ivan_latyshev@yahoo.com>
2021-06-03 06:40:03 +0300

    залил урок 12

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,10 @@
 $(function() {
 
+    $('.main-offer__slider').slick({
+        slidesToShow: 1,
+        arrows: false,
+        dots: true,
+    });
+
+    var mixer = mixitup('.products__list');
 });
